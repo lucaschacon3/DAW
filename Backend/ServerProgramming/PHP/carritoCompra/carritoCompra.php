685cd0e7b1a9c8d608ade1c6ef4a98a92c621e49
lucaschacon3 <l.chaconlanga3@gmail.com>
2024-10-23 11:10:55 +0200

    commit

--- Backend/ServerProgramming/PHP/carritoCompra/carritoCompra.php
+++ Backend/ServerProgramming/PHP/carritoCompra/carritoCompra.php
@@ -1,29 +1,30 @@
 <?php
 
-        $inventario = array(
-            "camiseta" => array(
-                "stock" => 10,
-                "precio" => 11.99
-            ),
 
-            "pantalon" => array(
-                "stock" => 15,
-                "precio" => 17.99
-            ),
+$inventario = array(
+    "camiseta" => array(
+        "stock" => 10,
+        "precio" => 11.99
+    ),
 
-            "sudadera" => array(
-                "stock" => 12,
-                "precio" => 15.25
-            ),
+    "pantalon" => array(
+        "stock" => 15,
+        "precio" => 17.99
+    ),
 
-            "zapatillas" => array(
-                "stock" => 16,
-                "precio" => 26.55
-            )
+    "sudadera" => array(
+        "stock" => 12,
+        "precio" => 15.25
+    ),
 
-        );
+    "zapatillas" => array(
+        "stock" => 16,
+        "precio" => 26.55
+    )
 
-        ?>
+);
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -46,29 +47,29 @@
 </head>
 
 <body>
-    <h2>Bienvenido Cliente!</h2>
+    <h2>Bienvenido <?php echo $_GET["nombre"] ?>!</h2>
     <form action="" method="post">
 
-        
+
 
         <label for="camiseta"> Camiseta: </label>
-        <input type="number" name="camiseta" id="camiseta">
+        <input type="number" name="camiseta" id="camiseta" value="0">
         <?php echo $inventario["camiseta"]["precio"] . " €"; ?>
         <br>
 
 
         <label for="pantalon"> Pantalon: </label>
-        <input type="number" name="pantalon" id="pantalon">
+        <input type="number" name="pantalon" id="pantalon" value="0">
         <?php echo $inventario["pantalon"]["precio"] . " €"; ?>
         <br>
 
         <label for="sudadera"> Sudadera: </label>
-        <input type="number" name="sudadera" id="sudadera">
+        <input type="number" name="sudadera" id="sudadera" value="0">
         <?php echo $inventario["sudadera"]["precio"] . " €"; ?>
         <br>
 
         <label for="zapatillas"> Zapatillas: </label>
-        <input type="number" name="zapatillas" id="zapatillas">
+        <input type="number" name="zapatillas" id="zapatillas" value="0">
         <?php echo $inventario["zapatillas"]["precio"] . " €"; ?>
         <br>
 
@@ -78,9 +79,27 @@
 
     <?php
 
+/*
+    define('RUTA_FICHERO', "stock.data");
+
+    if (file_exists(RUTA_FICHERO)) {
+        $contenido = file_get_contents(RUTA_FICHERO);
+        $stock = unserialize($contenido);
+    } else {
+        $stock = [
+            $inventario["camiseta"] => $inventario["camiseta"]["stock"],
+            $inventario["pantalon"] => $inventario["pantalon"]["stock"],
+            $inventario["sudadera"] => $inventario["sudadera"]["stock"],
+            $inventario["zapatillas"] => $inventario["zapatillas"]["stock"]
+        ];
+        file_put_contents(RUTA_FICHERO, serialize($stock));
+    }
+    */
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // ver stock
+        /* 
         $stockCamiseta = intval($inventario["camiseta"]["stock"]);
         $inventario["camiseta"]["stock"] = $stockCamiseta - intval($_REQUEST["camiseta"]);
 
@@ -92,51 +111,52 @@
 
         $stockZapatillas = intval($inventario["zapatillas"]["stock"]);
         $inventario["zapatillas"]["stock"] = $stockZapatillas - intval($_REQUEST["zapatillas"]);
+        */
     }
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (isset($_POST["actualizar"])) {
             // Mostrar resumen antes de confirmar
             echo "<h3>Resumen de pedido:</h3>";
-            $camiseta = isset($_REQUEST["camiseta"]) ? $_REQUEST["camiseta"] : 0;
-            $pantalon = isset($_REQUEST["pantalon"]) ? $_REQUEST["pantalon"] : 0;
-            $sudadera = isset($_REQUEST["sudadera"]) ? $_REQUEST["sudadera"] : 0;
-            $zapatillas = isset($_REQUEST["zapatillas"]) ? $_REQUEST["zapatillas"] : 0;
-            
 
-            echo "Camiseta: " . $camiseta . " Unidades <br>";
-            echo "Pantalon: " . $pantalon . " Unidades <br>";
-            echo "Sudadera: " . $sudadera . " Unidades <br>";
-            echo "Zapatillas: " . $zapatillas . " Unidades<br>";
+            echo "Camiseta: " . $_REQUEST["camiseta"] . " Unidades <br>";
+            echo "Pantalon: " . $_REQUEST["pantalon"] . " Unidades <br>";
+            echo "Sudadera: " . $_REQUEST["sudadera"] . " Unidades <br>";
+            echo "Zapatillas: " . $_REQUEST["zapatillas"] . " Unidades<br>";
             echo "<br>";
 
             $precioTotal = ($_REQUEST["camiseta"] * $inventario["camiseta"]["precio"]) + ($_REQUEST["pantalon"] * $inventario["pantalon"]["precio"]) + ($_REQUEST["sudadera"] * $inventario["sudadera"]["precio"]) + ($_REQUEST["zapatillas"] * $inventario["zapatillas"]["precio"]);
 
             echo "Precio Total: " . $precioTotal;
             echo "<br>";
-
+            echo "<form method=post>";
             echo "<button type=submit name=confirmar >CONFIRMAR PEDIDO</button>";
+            echo "</form>";
             echo "<br>";
         }
         if (!isset($_POST['confirmar'])) {
             // si no pulsas el botón de confirmación
 
             echo '<form action="" method="post">';
-            
+
             echo '</form>';
         } else {
             // Confirmación final, no volver a mostrar formulario
-            echo "<h3>¡Datos confirmados correctamente!</h3>";
-            echo '<a href="">Empezar desde cero</a>';
+            echo "<h3>¡Compra realizada!</h3>";
+            echo "<button><a href= >Volver a la tienda</a></button>";
+            echo "<br>";
         }
     }
-
+    /* 
     echo " ----STOCK---- <br>";
     foreach ($inventario as $producto => $stock) {
         echo $producto . " =>" . $stock["stock"] . "<br>";
     }
-    echo " ------------------ <br>"; 
+    echo " ------------------ <br>";
+    */
+    file_put_contents(RUTA_FICHERO, serialize($stock));
     ?>
+
 
 </body>
 
