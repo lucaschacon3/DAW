65fce77a98551e1b88dd71e71ae9cbe9e7cc6390
lucaschacon3 <l.chaconlanga3@gmail.com>
2024-10-25 11:11:37 +0200

    commit

--- Frontend/JavaScript/DOM/dom5/script.js
+++ Frontend/JavaScript/DOM/dom5/script.js
@@ -1,19 +1,30 @@
-
-function crearElemento(tipo, contenido, padre) {// padre= document.body
+function crearElemento(tipo, contenido, padre) {
+  // padre= document.body
   let hijo = document.createElement(tipo);
   hijo.innerHTML = contenido;
   padre.appendChild(hijo);
 
-  hijo.addEventListener(`click`, function eliminar () { 
+  hijo.addEventListener(`click`, function () {
     this.remove();
   });
 }
 
-let elementoCompra=document.getElementById(`idInputCompra`)
+let elementosCompra = document.getElementById(`idInputCompra`);
+let boton = document.querySelector(`button`);
 
-let boton= document.querySelector(`button`)
+boton.addEventListener(`click`, function agregarElemento() {
+  crearElemento(
+    `li`,
+    elementosCompra.value,
+    document.getElementById(`idElementosCompra`)
+  );
 
-boton.addEventListener(`click`, function eliminar () { 
-  crearElemento(`li`,elementoCompra.value, document.getElementById(`idElementosCompra`))
+  let productos=[]
 
-})
+  document.querySelectorAll(`div`).forEach((e)=>{
+    productos.push(e.textContent)
+  })
+
+  productos.short
+
+});
