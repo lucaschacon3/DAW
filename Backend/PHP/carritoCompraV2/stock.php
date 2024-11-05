4d684728df7b35519190085b3bd22ee22f2654fd
lucaschacon3 <l.chaconlanga3@gmail.com>
2024-11-05 01:57:36 +0100

    commit

--- Backend/PHP/carritoCompraV2/stock.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-include 'funciones.php';
-autenticarUsuario();   // Verifica autenticación
-verificarAdmin();       // Verifica permisos de administrador
-
-// Aquí va el resto del código de gestión de stock
-?>
