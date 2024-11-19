635f510e70abda1644a60b2b2cd75fd757839fea
lucaschacon3 <l.chaconlanga3@gmail.com>
2024-11-19 01:04:15 +0100

    commit

---
+++ Frontend/JavaScript/Practicas/practica3/script.js
@@ -0,0 +1,44 @@
+let ruta = `articulos.json`;
+
+fetch(ruta)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error, stattus ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    main(data);
+  })
+  .catch((error) => {
+    alert(`Error: ${error}`);
+  });
+
+function main(data) {
+    let select= document.getElementById(`idSelect`)
+    crearElemento(`option`,`Seleccione producto`, select)
+    data.forEach(e => {
+        crearElemento(`option`,`${e.nombre}`,select)
+        
+    });
+
+    let precio= document.getElementById(`precio`)
+    select.addEventListener(`change`, actualizaPrecio)
+    function actualizaPrecio() {
+        
+        precio.innerHTML=``
+        data.forEach((e)=>{
+            if (select.value===e.nombre) {
+                crearElemento(`p`, `El precio es ${e.precio}`, precio)
+            }
+        })
+    }
+
+
+}
+
+function crearElemento(tipo, contenido, padre) {
+    let hijo= document.createElement(tipo)
+    hijo.innerHTML=contenido
+    padre.appendChild(hijo)
+}
