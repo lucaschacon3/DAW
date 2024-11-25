2f9a0e843a0699305dea488c8ca16d69d7c70283
lucaschacon3 <l.chaconlanga3@gmail.com>
2024-11-25 10:18:09 +0100

    commit

---
+++ formulario/src/test/java/daw/goya/formulario/FormularioApplicationTests.java
@@ -0,0 +1,13 @@
+package daw.goya.formulario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormularioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
