b31bac6e7270fcd1d3e6ffb45f4abf31d6d330ab
lucaschacon3 <l.chaconlanga3@gmail.com>
2024-12-12 09:05:32 +0100

    commit

---
+++ Frontend/JavaScript/ReactJs/formularioAficiones/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './css/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Lucas Chacon</h1>
+    </>
+  )
+}
+
+export default App
