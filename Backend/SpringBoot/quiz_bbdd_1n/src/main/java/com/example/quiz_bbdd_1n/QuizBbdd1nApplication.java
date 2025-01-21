b5691c943e0ac6609cc49fc0e26188bc41fac006
lucaschacon3 <l.chaconlanga3@gmail.com>
2025-01-22 00:55:30 +0100

    commit

---
+++ Backend/SpringBoot/quiz_bbdd_1n/src/main/java/com/example/quiz_bbdd_1n/QuizBbdd1nApplication.java
@@ -0,0 +1,13 @@
+package com.example.quiz_bbdd_1n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizBbdd1nApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizBbdd1nApplication.class, args);
+	}
+
+}
---
+++ Backend/SpringBoot/quiz_bbdd_1n/src/test/java/com/example/quiz_bbdd_1n/QuizBbdd1nApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quiz_bbdd_1n;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizBbdd1nApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
