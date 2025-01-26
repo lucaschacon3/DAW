e09d3675d1ca793284aa9fd46198e28484149046
lucaschacon3 <l.chaconlanga3@gmail.com>
2025-01-26 21:37:21 +0100

    c

--- Frontend/JavaScript/ReactJs/5-Axios-Formularios/src/componentes/ListadoPrincipal.jsx
+++ Frontend/JavaScript/ReactJs/5-Axios-Formularios/src/componentes/ListadoPrincipal.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from "react";
 import ServicioAficiones from "../servicios/servicioAficiones.js";
-import "../estilos/Informe.css"
+import "../estilos/Informe.css";
 import Swal from "sweetalert2";
 import AficionCrear from "./crud-aficion/AficionCrear.jsx";
-import AficionEditar from "./crud-aficion/AficionEditar.jsx"
+import AficionEditar from "./crud-aficion/AficionEditar.jsx";
 import AficionConsultar from "./crud-aficion/AficionConsultar.jsx";
 import AficionBorrar from "./crud-aficion/AficionBorrar.js";
 import Modal from "./Modal.jsx";
-
 
 const ListadoPrincipal = () => {
   // Variables de Aficiones
@@ -21,11 +20,14 @@ const ListadoPrincipal = () => {
   });
 
   const gestionarModal = (tipoModal, estadoAbierto) => {
-      setModals((previoModals) => ({ ...previoModals, [tipoModal]: estadoAbierto }));
-    };
+    setModals((previoModals) => ({
+      ...previoModals,
+      [tipoModal]: estadoAbierto,
+    }));
+  };
 
   // ************************************************
-  // Carga inicial de todas las aficiones, 
+  // Carga inicial de todas las aficiones,
   // Gracias a UseEffect, se ejecuta una única vez
   // ************************************************
   useEffect(() => {
@@ -34,74 +36,93 @@ const ListadoPrincipal = () => {
         setAficiones(response.data);
       })
       .catch((error) => {
-
         Swal.fire({
           title: "¿Tienes Internet?",
           text: "No consigo descargar las aficiones :(",
-          icon: "question"
+          icon: "question",
         });
       });
   }, []);
 
   // ************************************************
-  // Manejadores de Eventos de : editar , crear y borrar 
+  // Manejadores de Eventos de : editar , crear y borrar
   // Gracias a UseEffect, se ejecuta una única vez
   // ************************************************
   const consultarAficion = (aficion) => {
     setAficionSeleccionada(aficion);
-    gestionarModal("consultar",true)
+    gestionarModal("consultar", true);
   };
 
   const EditarAficion = (aficion) => {
     setAficionSeleccionada(aficion);
-    gestionarModal("editar",true)
+    gestionarModal("editar", true);
   };
 
-  const crearAficion = () => {    
-    gestionarModal("crear",true)
+  const crearAficion = () => {
+    gestionarModal("crear", true);
   };
 
-  const borrarAficion = (aficion) =>{
-      AficionBorrar(aficion, aficiones, setAficiones)
-  }
-
+  const borrarAficion = (aficion) => {
+    AficionBorrar(aficion, aficiones, setAficiones);
+  };
 
   return (
-    <>      
+    <>
       <ul className="aficiones-list">
-  {aficiones.length > 0 ? (
-    aficiones.map((aficion) => (
-      <li key={aficion.id} className="aficion-item">
-        <div>
-          <strong>{aficion.nombre}</strong>: {aficion.descripcion}
-        </div>
-        <div>
-          <button onClick={() => consultarAficion(aficion)}>Consulta</button>
-          <button onClick={() => borrarAficion(aficion)}>Borrar</button>
-          <button onClick={() => EditarAficion(aficion)}>Editar</button>
-        </div>
-      </li>
-    ))
-  ) : (
-    <p>No se encontraron aficiones.</p>
-  )}
-</ul>
+        {aficiones.length > 0 ? (
+          aficiones.map((aficion) => (
+            <li key={aficion.id} className="aficion-item">
+              <div>
+                <strong>{aficion.nombre}</strong>: {aficion.descripcion}
+              </div>
+              <div>
+                <button onClick={() => consultarAficion(aficion)}>
+                  Consulta
+                </button>
+                <button onClick={() => borrarAficion(aficion)}>Borrar</button>
+                <button onClick={() => EditarAficion(aficion)}>Editar</button>
+              </div>
+            </li>
+          ))
+        ) : (
+          <p>No se encontraron aficiones.</p>
+        )}
+      </ul>
 
-<button className="add-aficion-btn" onClick={crearAficion}>Añadir Afición</button>
+      <button className="add-aficion-btn" onClick={crearAficion}>
+        Añadir Afición
+      </button>
 
+      <Modal
+        isOpen={modals.crear}
+        onClose={() => gestionarModal("crear", false)}
+      >
+        <AficionCrear
+          aficiones={aficiones}
+          setAficiones={setAficiones}
+          onClose={() => gestionarModal("crear", false)}
+        />
+      </Modal>
 
-      <Modal isOpen={modals.crear} onClose={()=>gestionarModal("crear",false)}>      
-          <AficionCrear aficiones={aficiones} setAficiones={setAficiones} onClose={()=>gestionarModal("crear",false)} />
-      </Modal>      
+      <Modal
+        isOpen={modals.consultar}
+        onClose={() => gestionarModal("consultar", false)}
+      >
+        {aficionSeleccionada && (
+          <AficionConsultar aficion={aficionSeleccionada} />
+        )}
+      </Modal>
 
-      <Modal isOpen={modals.consultar} onClose={()=>gestionarModal("consultar",false)}>
-              {aficionSeleccionada && <AficionConsultar aficion={aficionSeleccionada} />} 
-      </Modal> 
- 
-      <Modal isOpen={modals.editar} onClose={()=>gestionarModal("editar",false)} >
-           <AficionEditar  aficiones={aficionSeleccionada} setAficiones={setAficiones} onClose={()=>gestionarModal("editar",false)} />
-      </Modal>   
-   
+      <Modal
+        isOpen={modals.editar}
+        onClose={() => gestionarModal("editar", false)}
+      >
+        <AficionEditar
+          aficiones={aficionSeleccionada}
+          setAficiones={setAficiones}
+          onClose={() => gestionarModal("editar", false)}
+        />
+      </Modal>
     </>
   );
 };
