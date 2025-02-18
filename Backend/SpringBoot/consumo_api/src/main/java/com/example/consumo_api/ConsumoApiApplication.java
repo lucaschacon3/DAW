80e13a7e2bc2fb28323e7ceac1835dfda10ed7c9
lucaschacon3 <l.chaconlanga3@gmail.com>
2025-02-18 10:47:18 +0100

    commit

---
+++ Backend/SpringBoot/consumo_api/src/main/java/com/example/consumo_api/ConsumoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.consumo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumoApiApplication.class, args);
+	}
+
+}
---
+++ Backend/SpringBoot/consumo_api/src/test/java/com/example/consumo_api/ConsumoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
